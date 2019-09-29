65009292f1777a14caab25c2aad1853fbf86d86d
Rails to Cosmos <akatovda@yandex.com>
2019-09-29 17:17:13 +0300

    Add ogpm stub.

---
+++ apps/org-glance-password-manager.el
@@ -0,0 +1,11 @@
+(defvar og-pm-cache-file "~/.emacs.d/org-glance/passwords.el")
+
+(defun org-glance-password-manager-visit ()
+  (interactive)
+  (let ((org-glance-prompt "Visit secure data: ")
+        (org-glance-cache og-pm-cache-file)
+        (org-glance-fallback (lambda (x) (user-error "Entry not found.")))
+        (org-glance-title-property :TITLE)
+        (org-glance-filter (lambda (headline)
+                             (-contains? (org-element-property :tags headline) "Password"))))
+    (org-glance 'agenda-with-archives)))
