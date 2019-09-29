9e425de3c08a37247a39e187d7fd1bf899006837
Rails to Cosmos <akatovda@yandex.com>
2019-09-29 17:09:16 +0300

    Explicit cache reread.

--- apps/org-glance-bookmark.el
+++ apps/org-glance-bookmark.el
@@ -1,8 +1,10 @@
+(defvar og-bmkp-cache-file "~/.emacs.d/org-glance/bookmarks.el")
+
 ;;;###autoload
 (defun org-glance-bookmark-jump ()
   (interactive)
   (let ((org-glance-prompt "Jump to bookmark: ")
-        (org-glance-cache "~/.emacs.d/org-glance/bookmarks.el")
+        (org-glance-cache og-bmkp-cache-file)
         (org-glance-fallback (lambda (x) (user-error "Bookmark not found.")))
         (org-glance-title-property :TITLE)
         (org-glance-action #'og-act--open-org-link)
@@ -15,12 +17,18 @@
 (defun org-glance-bookmark-visit ()
   (interactive)
   (let ((org-glance-prompt "Visit bookmark: ")
-        (org-glance-cache "~/.emacs.d/org-glance/bookmarks.el")
+        (org-glance-cache og-bmkp-cache-file)
         (org-glance-fallback (lambda (x) (user-error "Bookmark not found.")))
         (org-glance-title-property :TITLE)
         (org-glance-filter (lambda (headline)
                              (and (s-matches? org-bracket-link-regexp (org-element-property :raw-value headline))
                                   (-contains? (org-element-property :tags headline) "Bookmark")))))
     (org-glance 'agenda-with-archives)))
+
+;;;###autoload
+(defun org-glance-bookmark-reread ()
+  (interactive)
+  (delete-file og-bmkp-cache-file)
+  (org-glance-bookmark-jump))
 
 (provide 'org-glance-bookmark)
