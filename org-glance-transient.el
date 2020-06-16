1313831298a480a6e23d7f8197a544a3ec479ecd
Dmitry Akatov <akatovda@yandex.com>
2020-06-16 12:54:33 +0300

    minor fixes

--- org-glance-transient.el
+++ org-glance-transient.el
@@ -1,55 +1,54 @@
 (require 'transient)
 (require 'eieio-core)
 
-(defclass org-glance--variable (transient-variable)
-  ((scope       :initarg :scope)))
+(defclass org-glance--variable (transient-variable) ())
+
+(cl-defmethod transient-init-value ((obj org-glance--variable))
+  (let ((variable (format (oref obj variable))))
+    (oset obj variable variable)))
+
+(cl-defmethod transient-infix-set ((obj org-glance--variable) value)
+  (let ((variable (oref obj variable)))
+    (oset obj value value)
+    (set (make-local-variable (intern (oref obj variable))) value)
+    (unless (or value transient--prefix)
+      (message "Unset %s" variable))))
+
+(cl-defmethod transient-format-description ((obj org-glance--variable))
+  (or (oref obj description)
+      (oref obj variable)))
+
+(cl-defmethod transient-format-value ((obj org-glance--variable))
+  (propertize (or (oref obj value) "Hey") 'face 'transient-inactive-value))
 
 (defclass org-glance--variable:view (org-glance--variable)
-  ((choices     :initarg :choices)
-   (default     :initarg :default     :initform nil)))
+  ())
 
 (cl-defmethod transient-infix-read ((obj org-glance--variable:view))
-  (let ((choices (oref obj choices)))
-    (when (functionp choices)
-      (setq choices (funcall choices)))
-    (if-let ((value (oref obj value)))
-        (cadr (member value choices))
-      (car choices))))
+  (oset obj value (org-glance-read-view "View: ")))
 
 (cl-defmethod transient-format-value ((obj org-glance--variable:view))
-  (let* ((variable (oref obj variable))
-         (choices (oref obj choices)))
-      "Hello"))
+  (propertize (or (oref obj value) "ALL") 'face 'transient-argument))
+
+;; (transient-define-infix org-glance-act.current-view ()
+;;   :class 'org-glance--variable:view
+;;   :variable "--current-view"
+;;   :reader 'org-glance-read-test)
 
 ;;;###autoload (autoload 'org-glance-act "org-glance" nil t)
 (transient-define-prefix org-glance-act (view)
   "In Glance-View buffer, perform action on selected view"
-  [ ;; "Arguments"
-   ("-r" "Reread database from scope" "--reread")
-   ;; (org-glance-arg:--view)
-   ("V" org-glance-act.current-view)
+  ["Arguments"
+   ("-r" "Rebuild database file" "--reread")
+   ;; ("-v" org-glance-act.current-view)
    ]
   ["Actions"
    [("j" "Jump"        org-glance-action-open)]
    [("m" "Materialize" org-glance-action-materialize)]
    [("v" "Visit"       org-glance-action-visit)]
-   [("d" "Decrypt"     org-glance-action-decrypt)]]
-  ;; (interactive (list (org-glance-read-view "Read view: ")))
-  ;; (transient-setup 'org-glance-act nil nil :scope view)
+   [("d" "Decrypt"     org-glance-action-decrypt)]
+   ;; [("t" "Test"        org-glance-action-test)]
+   ]
   )
-
-;; (transient-define-argument org-glance-arg:--view ()
-;;   :description "View"
-;;   :class 'transient-option
-;;   :shortarg "-v"
-;;   :argument "--view="
-;;   :choices org-glance-views)
-
-(transient-define-infix org-glance-act.current-view ()
-  :class 'org-glance--variable:view
-  :scope 'magit--read-branch-scope
-  :variable "act.current-view"
-  :choices '("Book" "Bookmark")         ;org-glance-views
-  :default "all")
 
 (provide-me)
