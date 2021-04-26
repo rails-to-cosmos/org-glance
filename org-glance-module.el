ab6bc0b202ac229b868a9ff6060e1a379a5e4de5
Dmitry Akatov <akatovda@yandex.com>
2021-04-26 11:33:35 +0300

    Minor refactor.

--- org-glance-module.el
+++ org-glance-module.el
@@ -2,15 +2,15 @@
 (require 'f)
 (require 'files)
 
-(defvar org-glance-root-directory (file-name-directory (f-join (__FILE__) "..")))
+(defvar org-glance-module-root-directory (file-name-directory (f-join (__FILE__) "..")))
 
 (defmacro org-glance-module-import (module)
   `(let* ((m (format "org-glance.%s" (quote ,module)))
           (path (concat (s-replace "." "/" m) ".el")))
-     (require (intern m) (f-join org-glance-root-directory path))))
+     (require (intern m) (f-join org-glance-module-root-directory path))))
 
 (defmacro org-glance-module-provide ()
   `(provide (intern (file-name-sans-extension
-                     (s-replace "/" "." (file-relative-name (__FILE__) org-glance-root-directory))))))
+                     (s-replace "/" "." (file-relative-name (__FILE__) org-glance-module-root-directory))))))
 
 (provide 'org-glance-module)
