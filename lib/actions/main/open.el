e7b97909c2f9f568ff62c8ef1d17d36431dfb4ac
Dmitry Akatov <akatovda@yandex.com>
2021-06-08 17:20:44 +0300

    Minor cleanup.

--- lib/actions/main/open.el
+++ lib/actions/main/open.el
@@ -1,25 +1,18 @@
 (require 'org-glance-module)
 
-(org-glance-module-import lib.core.metastore)
 (org-glance-module-import lib.core.actions)
-(org-glance-module-import lib.core.view)
+(org-glance-module-import lib.core.headline)
 
 (org-glance-action-define open (headline) :for link
   "Visit HEADLINE, then search for all `org-any-link-re' in its subtree.
 Ask with `org-completing-read' and apply `org-open-at-point' on it."
-  (org-glance-with-headline-narrowed headline
-      (let* ((links (org-element-map (org-element-parse-buffer) 'link
-                      (lambda (link)
-                        (cons
-                         (substring-no-properties
-                          (or (nth 2 link) ;; link alias
-                              (org-element-property :raw-link link))) ;; full link if alias is none
-                         (org-element-property :begin link)))))
-             (point (cond
-                      ((> (length links) 1) (cdr (assoc (org-completing-read "Open link: " links) links)))
-                      ((= (length links) 1) (cdar links))
-                      (t (user-error "Unable to find links in %s" (buffer-file-name))))))
-        (goto-char point)
-        (org-open-at-point))))
+  (let* ((links (org-glance-headline:links headline))
+         (pos (cond
+                ((> (length links) 1) (cdr (assoc (org-completing-read "Open link: " links) links)))
+                ((= (length links) 1) (cdar links))
+                (t (user-error "Unable to find links in %s" (org-glance-headline:file headline))))))
+    (org-glance-with-headline-narrowed headline
+      (goto-char pos)
+      (org-open-at-point))))
 
 (org-glance-module-provide)
--- lib/core/headline.el
+++ lib/core/headline.el
@@ -96,21 +96,22 @@ Raise `org-glance-headline-not-found` error on fail.''"
 
 (defmacro org-glance-with-headline-narrowed (headline &rest forms)
   "Visit HEADLINE, narrow to its subtree and execute FORMS on it."
-  (declare (indent defun))
+  (declare (indent 1) (debug t))
   `(let* ((file (org-element-property :file ,headline))
           (file-buffer (get-file-buffer file))
-          (visited-buffer (current-buffer)))
+          (visited-buffer (current-buffer))
+          res)
      (org-glance-headline:visit ,headline)
-     (widen)
      (org-narrow-to-subtree)
      (unwind-protect
-          (let ((org-link-frame-setup (cl-acons 'file 'find-file org-link-frame-setup)))
-            ,@forms)
+          (setq res (let ((org-link-frame-setup (cl-acons 'file 'find-file org-link-frame-setup)))
+                      ,@forms))
        (widen))
      (cond ((and file-buffer (not (eq file-buffer (current-buffer)))) (bury-buffer file-buffer))
            ((and file-buffer (eq file-buffer (current-buffer))) (progn (switch-to-buffer visited-buffer)
                                                                        (bury-buffer file-buffer)))
-           (t (kill-buffer (get-file-buffer file))))))
+           (t (kill-buffer (get-file-buffer file))))
+     res))
 
 (cl-defun org-glance-headline:contents (headline)
   (save-window-excursion
@@ -118,5 +119,15 @@ Raise `org-glance-headline-not-found` error on fail.''"
       (org-glance-headline:visit headline)
       (org-narrow-to-subtree)
       (s-replace-regexp "^\\*+" "*" (buffer-substring-no-properties (point-min) (point-max))))))
+
+(cl-defun org-glance-headline:links (headline)
+  (org-glance-with-headline-narrowed headline
+    (org-element-map (org-element-parse-buffer) 'link
+      (lambda (link)
+        (cons
+         (substring-no-properties
+          (or (nth 2 link) ;; link alias
+              (org-element-property :raw-link link))) ;; full link if alias is none
+         (org-element-property :begin link))))))
 
 (org-glance-module-provide)
