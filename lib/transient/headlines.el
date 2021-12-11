4d90ffbb4585c32e4ac3d8d63bf9e84c07424258
dmitry.akatov <dmitry.akatov@team.wrike.com>
2021-12-11 15:30:20 +0300

    cyrillic

--- lib/transient/headlines.el
+++ lib/transient/headlines.el
@@ -20,4 +20,19 @@
   (org-glance:init)
   (transient-setup 'org-glance-form-action))
 
+(transient-define-prefix org-glance-form-action--cyrillic ()
+  "Perform action on selected view/headlines"
+  ["Overview"
+   [("ф" "Agenda" org-glance-overview:agenda*)
+    ("щ" "Overview" org-glance-overview)]]
+  ["Headline actions"
+   [("+" "Capture" org-glance:capture)
+    ("у" "Extract" org-glance:extract)
+    ("л" "Kill" org-glance:revoke)
+    ("о" "Jump" org-glance:open)
+    ("ь" "Materialize" org-glance:materialize)]]
+  (interactive)
+  (org-glance:init)
+  (transient-setup 'org-glance-form-action--cyrillic))
+
 (org-glance:provide)
