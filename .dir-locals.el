e50bf03994b4935c831d6558dfdbdd814eba9429
Dmitry Akatov <akatovda@yandex.com>
2022-02-04 11:02:10 +0300

    dirlocals cleanup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((flycheck-emacs-lisp-load-path . inherit)
-                     ;; (eval . (let
-                     ;;             ((project-directory
-                     ;;               (expand-file-name
-                     ;;                (locate-dominating-file default-directory "org-glance.el"))))
-                     ;;           (setq-local flycheck-emacs-lisp-load-path 'inherit)))
-                     )))
+((emacs-lisp-mode . ((flycheck-emacs-lisp-load-path . inherit))))
