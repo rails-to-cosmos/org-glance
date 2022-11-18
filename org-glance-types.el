b17f960279cc16887f630a138a68596c8746ffa0
Dmitry Akatov <akatovda@gmail.com>
2022-11-18 10:09:09 +0300

    type subst

--- org-glance-types.el
+++ org-glance-types.el
@@ -6,28 +6,29 @@
 
 (defalias 's-replace-regexp 'replace-regexp-in-string)
 
+(cl-defun org-glance-type-subst (type)
+  (pcase type
+    ((and T
+          (cl-struct symbol)
+          (guard (<= 65 (aref (symbol-name T) 0) 90)))
+     (read (format "org-glance-%s"
+                   (let ((case-fold-search nil))
+                     (downcase (s-replace-regexp "\\([a-z]\\)\\([A-Z]\\)" "\\1-\\2" (symbol-name T)))))))
+    ((and T (cl-struct list)) (-map #'org-glance-type-subst T))
+    (_ type)))
+
 (cl-defmacro org-glance-fun (name args _ return-type &rest body)
   (declare (indent 4))
-  (cl-labels ((type-subst (type)
-                (pcase type
-                  ((and T
-                        (cl-struct symbol)
-                        (guard (<= 65 (aref (symbol-name T) 0) 90)))
-                   (read (format "org-glance-%s"
-                                 (let ((case-fold-search nil))
-                                   (downcase (s-replace-regexp "\\([a-z]\\)\\([A-Z]\\)" "\\1-\\2" (symbol-name T)))))))
-                  ((and T (cl-struct list)) (-map #'type-subst T))
-                  (_ type))))
-    (cl-loop
-       for (arg _ type) in args
-       collect arg into cl-args
-       collect (list arg type) into cl-types
-       finally return (append `(cl-defun ,name)
-                              (list cl-args)
-                              (cl-loop for (arg type) in cl-types
-                                 collect `(cl-check-type ,arg ,(type-subst type)))
-                              (list `(cl-the ,(type-subst return-type)
-                                       (progn ,@body)))))))
+  (cl-loop
+     for (arg _ type) in args
+     collect arg into cl-args
+     collect (list arg type) into cl-types
+     finally return (append `(cl-defun ,name)
+                            (list cl-args)
+                            (cl-loop for (arg type) in cl-types
+                               collect `(cl-check-type ,arg ,(org-glance-type-subst type)))
+                            (list `(cl-the ,(org-glance-type-subst return-type)
+                                     (progn ,@body))))))
 
 ;; (cl-macroexpand '(org-glance-fun hello ((a :: (Optional Hash))) -> HashSum
 ;;                   (+ 1 a)))
