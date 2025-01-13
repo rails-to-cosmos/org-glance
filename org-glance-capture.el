15cd664fb25e85f58839355dbd6ca93a4841b7a1
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-01-13 11:29:45 +0100

    capture wip

---
+++ org-glance-capture.el
@@ -0,0 +1,61 @@
+;; -*- lexical-binding: t -*-
+
+(cl-defun org-glance-capture1 (tag title &key (callback nil) (template nil))
+  (declare (indent 2))
+
+  (interactive (list (let ((tag (org-glance-tags:completing-read "Tag: " nil)))
+                       (org-glance:create-tag tag))
+                     (cond ((use-region-p) (buffer-substring-no-properties (region-beginning) (region-end)))
+                           (t ""))))
+
+  (let ((id (org-glance-tag:generate-id tag))
+        (file (make-temp-file "org-glance-" nil ".org")))
+
+    (cl-flet ((prepare-finalize-hook ()
+                (org-glance-headline1--from-string (buffer-string)) ;; should create graph of headlines in general
+                ;; (goto-char (point-min))
+                ;; (or (org-at-heading-p) (org-next-visible-heading 0))
+                (org-set-property "ORG_GLANCE_ID" id)
+
+                (org-toggle-tag (format "%s" tag) t))
+
+              (after-finalize-hook ()
+                (when-let (headline (org-glance-headline:search-buffer-by-id id))
+                  (let* ((title (org-glance-headline:plain-title headline))
+                         (tag-file (org-glance:tag-file-name tag))
+                         (refile-dir (org-glance-headline:make-directory tag-file title))
+                         (tmp-file (org-glance-headline:file-name headline))
+                         (new-file (org-glance--make-file-directory (f-join refile-dir (format "%s.org" tag)))))
+                    ;; (org-set-property "DIR" (abbreviate-file-name refile-dir))
+                    ;; (save-buffer)
+                    ;; (kill-buffer)
+
+                    (f-move tmp-file new-file)
+                    (org-glance-headline:update headline
+                      :file new-file
+                      :dir (abbreviate-file-name refile-dir))
+                    (org-glance-overview tag)
+                    (org-glance-overview:register-headline-in-metadata headline tag)
+                    (org-glance-overview:register-headline-in-overview headline tag)
+                    ;; (org-overview)
+                    ;; (org-glance-headline:search-buffer-by-id id)
+                    )))
+              )
+      )
+    (find-file file)
+
+    (add-hook 'org-capture-prepare-finalize-hook (lambda () )
+              0 t)
+
+    (add-hook 'org-capture-after-finalize-hook (lambda () (org-glance-capture:after-finalize-hook id tag)) 0 t)
+    (when callback (add-hook 'org-capture-after-finalize-hook callback 1 t))
+
+    (let ((org-capture-templates (list (list "_" "_" 'entry (list 'file file) template))))
+      (org-capture nil "_")
+      (when finalize (org-capture-finalize)))
+
+    id)
+
+  )
+
+(provide 'org-glance-capture)
