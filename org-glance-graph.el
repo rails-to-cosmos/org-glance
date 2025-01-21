d41b08e7fddcaaf6976c37f0082ff5c83bc65bbd
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-01-21 21:40:28 +0100

    meta reader

--- org-glance-graph.el
+++ org-glance-graph.el
@@ -18,6 +18,7 @@
 
 (cl-defstruct (org-glance-headline1-metadata (:predicate org-glance-headline1-metadata?)
                                              (:conc-name org-glance-headline1-metadata:))
+  (graph nil :read-only t :type org-glance-graph)
   (id nil :read-only t :type string)
   (state nil :read-only t :type string)
   (title nil :read-only t :type string)
@@ -29,11 +30,51 @@
   (relations nil :read-only t :type list)
   (tombstone nil :read-only t :type boolean))
 
-(cl-defun org-glance-headline1-metadata:serialize (metadata graph &key (id (org-glance-headline1-metadata:id metadata)))
-  (cl-check-type metadata org-glance-headline1-metadata)
-  (cl-check-type id string)
+(cl-defun org-glance-headline1-metadata (graph headline)
   (cl-check-type graph org-glance-graph)
-  (->> (list :id id
+  (cl-check-type headline org-glance-headline1)
+  (make-org-glance-headline1-metadata
+   :graph graph
+   :id (or (org-glance-headline1:id headline)
+           (org-glance-graph:make-id graph))
+   :state (org-glance-headline1:state headline)
+   :title (org-glance-headline1:title headline)
+   :tags (org-glance-headline1:tags headline)
+   :hash (org-glance-headline1:hash headline)
+   :schedule (org-glance-headline1:schedule headline)
+   :deadline (org-glance-headline1:deadline headline)
+   :priority (org-glance-headline1:priority headline)
+   :relations nil
+   :tombstone nil))
+
+(cl-defmacro org-glance-jsonl:--iter-lines (file &rest forms)
+  (declare (indent 1))
+  `(with-temp-buffer
+     (cl-flet ((process-line () (let ((it (json-parse-string (buffer-substring-no-properties (line-beginning-position) (line-end-position))
+                                                             :object-type 'plist)))
+                                  ;; (message (prin1-to-string it))
+                                  ,@forms)))
+       (cl-loop with result = nil
+                with chunk-size = 50
+                with file-size = (file-attribute-size (file-attributes ,file))
+                with disk-read-count = 0
+                for upper-bound downfrom file-size downto 0 by chunk-size
+                for lower-bound = (max 0 (- upper-bound chunk-size))
+                do (progn (cl-incf disk-read-count)
+                          (goto-char (point-min))
+                          (insert-file-contents-literally ,file nil lower-bound upper-bound)
+                          (goto-char (point-max))
+                          (skip-chars-backward "\n")
+                          (beginning-of-line)
+                          (while (not (or (bobp) (setq result (process-line))))
+                            (delete-line)
+                            (forward-line -1)
+                            (beginning-of-line)))
+                finally return (or result (process-line))))))
+
+(cl-defun org-glance-headline1-metadata:serialize (metadata)
+  (cl-check-type metadata org-glance-headline1-metadata)
+  (->> (list :id (org-glance-headline1-metadata:id metadata)
              :title (org-glance-headline1-metadata:title metadata)
              :hash (org-glance-headline1-metadata:hash metadata)
              :relations (org-glance-headline1-metadata:relations metadata))
@@ -53,16 +94,17 @@
   `(with-mutex (org-glance-graph:mutex ,graph)
      ,@forms))
 
-(cl-defun org-glance-graph:headline-data-directory (graph id)
+(cl-defun org-glance-graph:headline-data-path (graph id)
+  "TODO move to metadata."
   (cl-check-type graph org-glance-graph)
   (cl-check-type id string)
   (f-join (org-glance-graph:data-path graph) (substring id 0 2) (substring id 2)))
 
-(cl-defun org-glance-graph:data-directory (graph)
+(cl-defun org-glance-graph:data-path (graph)
   (cl-check-type graph org-glance-graph)
   (f-join (org-glance-graph:directory graph) "data"))
 
-(cl-defun org-glance-graph:meta-directory (graph)
+(cl-defun org-glance-graph:meta-path (graph)
   (cl-check-type graph org-glance-graph)
   (f-join (org-glance-graph:directory graph) "meta"))
 
@@ -71,36 +113,38 @@
   (org-glance-graph:modify graph
     (cl-loop while t
              for id = (org-id-uuid)
-             for data-directory = (org-glance-graph:headline-data-directory graph id)
-             unless (f-exists? data-directory)
-             return (prog1 id (f-mkdir-full-path data-directory)))))
+             for data-path = (org-glance-graph:headline-data-path graph id)
+             unless (f-exists? data-path)
+             return (prog1 id (f-mkdir-full-path data-path)))))
+
+(cl-defun org-glance-headline1-metadata:data-path (metadata)
+  (cl-check-type metadata org-glance-headline1-metadata)
+  (f-join (org-glance-graph:headline-data-path (org-glance-headline1-metadata:graph metadata) (org-glance-headline1-metadata:id metadata)) "data.org"))
 
 (cl-defun org-glance-graph:add-headline-metadata (graph metadata)
-  "Set headline METADATA in GRAPH."
+  "Set headline METADATA in GRAPH. TODO return a new graph."
   (cl-check-type graph org-glance-graph)
   (cl-check-type metadata org-glance-headline1-metadata)
-  (let ((id (or (org-glance-headline1-metadata:id metadata)
-                (org-glance-graph:make-id graph)))
-        (data (org-glance-headline1-metadata:serialize metadata graph :id id)))
-    (org-glance-graph:modify graph
-      (f-append-text data `utf-8 (f-join (org-glance-graph:meta-directory graph) "headlines.jsonl"))
-      (with-temp-file (f-join (org-glance-graph:headline-data-directory graph id) "data.org")
-        (insert (org-glance-headline1:contents headline))))
-    id))
+  (cl-assert (equal graph (org-glance-headline1-metadata:graph metadata)))
+  (org-glance-graph:modify graph
+    (f-append-text (org-glance-headline1-metadata:serialize metadata) `utf-8 (f-join (org-glance-graph:meta-path graph) "headlines.jsonl"))))
+
+(cl-defun org-glance-graph:get-headline-metadata (graph id)
+  (cl-check-type graph org-glance-graph)
+  (cl-check-type id string)
+
+  (org-glance-jsonl:--iter-lines (f-join (org-glance-graph:meta-path graph) "headlines.jsonl")
+    (when (string= (plist-get it :id) id)
+      it)))
 
 (cl-defun org-glance-graph:remove-headline-metadata (graph id)
   (cl-check-type graph org-glance-graph)
   (cl-check-type id string)
   )
 
-(cl-defun org-glance-graph:get-headline-metadata (graph id)
-  (cl-check-type graph org-glance-graph)
-  (cl-check-type id string)
-  )
-
 (cl-defun org-glance-graph:add-relation (graph relation a-id b-id)
   (cl-check-type graph org-glance-graph)
-  (cl-check-type relation string)
+  (cl-check-type relation symbol)
   (cl-check-type a-id string)
   (cl-check-type b-id string)
   (let* ((a (org-glance-graph:get-headline-metadata graph a-id))
@@ -110,36 +154,25 @@
     (org-glance-graph:set-headline-metadata graph :id a-id :relations (cl-pushnew b-id a-relations))
     (org-glance-graph:set-headline-metadata graph :id b-id :relations (cl-pushnew a-id b-relations))))
 
-(cl-defmacro org-glance-jsonl:--iter-lines (file &rest forms)
-  (declare (indent 1))
-  `(with-temp-buffer
-     (cl-flet ((process-line () (let ((it (json-parse-string (buffer-substring-no-properties (line-beginning-position) (line-end-position)) :object-type 'plist)))
-                                  ,@forms)))
-       (cl-loop with chunk-size = 4096
-                with file-size = (file-attribute-size (file-attributes ,file))
-                with disk-read-count = 0
-                for upper-bound downfrom file-size downto 0 by chunk-size
-                for lower-bound = (max 0 (- upper-bound chunk-size))
-                do (progn (incf disk-read-count)
-                          (goto-char (point-min))
-                          (insert-file-contents-literally ,file nil lower-bound upper-bound)
-                          (goto-char (point-max))
-                          (skip-chars-backward "\n")
-                          (beginning-of-line)
-                          (while (not (bobp))
-                            (process-line)
-                            (delete-line)
-                            (forward-line -1)
-                            (beginning-of-line)))
-                finally (process-line)
-                (message "disk read count: %d, chunk size: %d" disk-read-count chunk-size)))))
-
 ;; (let* ((graph (org-glance-graph "/tmp/glance"))
 ;;        (foo (org-glance-headline1--from-lines "* foo" "- [[http://10.17.2.107:3002/overview/activity/timeline][Web UI]]"))
 ;;        (bar (org-glance-headline1--from-lines "* bar" "123"))
-;;        (foo-id (org-glance-graph:set-headline graph foo))
-;;        (bar-id (org-glance-graph:set-headline graph bar
-;;                                               :id foo-id)))
-;;   (org-glance-graph:add-relation graph "parent" foo-id bar-id))
+;;        (foo-meta (org-glance-headline1-metadata graph foo))
+;;        (bar-meta (org-glance-headline1-metadata graph bar))
+;;        ;; (foo-id (org-glance-graph:set-headline-metadata graph foo))
+;;        ;; (bar-id (org-glance-graph:set-headline-metadata graph bar :id foo-id))
+;;        )
+;;   ;; (org-glance-graph:add-relation graph "parent" foo-id bar-id)
+
+;;   (org-glance-graph:add-headline-metadata graph foo-meta)
+;;   (org-glance-graph:add-headline-metadata graph bar-meta)
+
+;;   ;; (org-glance-graph:add-relation graph 'parent
+;;   ;;                                (org-glance-headline1-metadata:id foo-meta)
+;;   ;;                                (org-glance-headline1-metadata:id bar-meta))
+;;   )
+
+;; (let ((graph (org-glance-graph "/tmp/glance")))
+;;   (org-glance-graph:get-headline-metadata graph "60be9868-1017-4085-a03e-c83fdaee0cb1"))
 
 (provide 'org-glance-graph)
