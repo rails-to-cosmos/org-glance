9c93990a2ff19fa9afce714caa868cb161747bde
Dmitry Akatov <dmitry.akatov@protonmail.com>
2025-01-24 19:34:51 +0100

    minor

--- org-glance-capture-v2.el
+++ org-glance-capture-v2.el
@@ -25,11 +25,10 @@
          (org-capture-templates (list (list temp-capture-token temp-capture-token 'entry (list 'file file) template))))
     (find-file file)
     (add-hook 'org-capture-after-finalize-hook
-              #'(lambda ()
-                  (unwind-protect
-                      (org-glance-graph-v2:capture-buffer)
-                    (kill-buffer (get-file-buffer file))
-                    (f-delete file)))
+              #'(lambda () (unwind-protect
+                          (org-glance-graph-v2:capture-buffer)
+                        (kill-buffer (get-file-buffer file))
+                        (f-delete file)))
               0 t)
     (org-capture nil temp-capture-token)
     ;; (when finalize (org-capture-finalize))
