092c684e42129962ec6940a55434ff7eb2a631fe
Dmitry Akatov <dmitry.akatov@protonmail.com>
2026-07-12 22:56:05 +0200

    versioning

--- org-glance.el
+++ org-glance.el
@@ -6,7 +6,7 @@
 
 ;; Author: Dmitry Akatov <dmitry.akatov@protonmail.com>
 ;; Created: 29 September, 2018
-;; Version: 0.2.1.0.20260712.0
+;; Version: 0.2.1
 ;; Package-Requires: ((emacs "29.1") (org) (aes) (dash) (f) (s) (transient) (table-view "0"))
 ;; Keywords: org-mode, graph, mindmap
 ;; Homepage: https://github.com/rails-to-cosmos/org-glance
